d3d2956dd67f595355a5641ffd434affbc041360
MrXck <1971877559@qq.com>
2024-03-31 17:19:35 +0800

    修改登录接口配置

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,7 +1,11 @@
 import request from "@/utils/request";
 
 export function loginApi(data) {
-    return request.post("/user/login", data)
+    return request.post("/user/login", data, {
+        headers: {
+            "Content-Type": 'application/x-www-form-urlencoded;charset=UTF-8'
+        }
+    })
 }
 
 export function registerApi(data) {
